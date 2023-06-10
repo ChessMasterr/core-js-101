1bee9468f953851e3fc5f57984513e142ac88e26
ChessMasterr <artiom.farhutdinoff@yandex.ru>
2023-06-11 01:20:18 +0500

    feat: answer for 03

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -486,8 +486,10 @@ function getIdentityMatrix(/* n */) {
  *     0, 100 => [ 0, 1, 2, ..., 100 ]
  *     3, 3   => [ 3 ]
  */
-function getIntervalArray(/* start, end */) {
-  throw new Error('Not implemented');
+function getIntervalArray(start, end) {
+  let arr = new Array(end - start + 1);
+  arr = arr.fill(start);
+  return arr.map((el, i) => el + i);
 }
 
 /**
